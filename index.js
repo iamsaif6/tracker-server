ff7f487d6a599995be6c00799558bfb5f78fe9d9
Saif Hasan <saifhassan399@gmail.com>
2024-04-28 19:50:40 +0600

    User collection form link to server

--- index.js
+++ index.js
@@ -4,6 +4,48 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mal3t53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db('touristDB');
+    const spotCollections = database.collection('spotCollections');
+
+    // Add Spot data via user form
+    app.post('/add_spot', async (req, res) => {
+      const spots = req.body;
+      const result = await spotCollections.insertOne(spots);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+// Server Request and responses
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
